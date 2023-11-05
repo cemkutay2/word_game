79500e11c4fbdc1290fd041cbe11ac87dcdede98
cemkutay2 <c.kutay.ates@gmail.com>
2023-11-05 11:23:55 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+int main()
+{
+    bool running = true;
+    do{
+        const char *words[] = {"cat", "dog", "house", "tree", "flower", "car", "bird", "fish", "apple", "orange", "banana", "grape", "strawberry", "blueberry", "lemon", "lime", "watermelon", "sun", "moon", "star", "rain", "snow"};
+
+        // rng
+        time_t t;
+        srand((unsigned)time(&t));
+        int randNum = (rand() % (sizeof(words) / sizeof(words[0])));
+
+        // get a random word from the words array
+        const char *tempChar = words[randNum];
+        char randWord[100];
+        strcpy(randWord, tempChar);
+        // 
+        int wordLength = strlen(randWord);
+        char wordSpace[wordLength];
+        for (int i = 0; i < wordLength; i++)
+        {
+            wordSpace[i] = '_';
+        }
+
+        // game loop
+        bool wordIsFound = false;
+        bool wrongGuess = false;
+        do
+        {
+            // clear screen
+            printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+            printf(wrongGuess ? "Wrong Guess! Guess again:\n" : "Guess:\n");
+            wrongGuess = false;
+            char userGuess[100];
+            // print out the main array
+            for (int i = 0; i < wordLength; i++)
+            {
+                printf("%c ", wordSpace[i]);
+            }
+            // take a guess
+            printf("\n\n");
+            scanf("%s", &userGuess[0]);
+
+            int inputLength = strlen(userGuess);
+            char currentLetter = userGuess[0];
+            
+            // if word is found end loop
+            if (strcmp(userGuess, randWord) == 0)
+            {
+                wordIsFound = true;
+                continue;
+            }
+            else if (inputLength > 1)
+            {
+                wrongGuess = true;
+                continue;
+            }
+            // change the main array
+            for (int i = 0; i < wordLength; i++)
+            {
+                if (currentLetter == randWord[i])
+                {
+                    wordSpace[i] = currentLetter;
+                }
+            }
+        } while (!wordIsFound);
+        printf("Congrats! The word was: %s!\n",randWord);
+        char playAgain;
+        printf("\nDo you want to play again? (y/n)\n");
+        scanf(" %c", &playAgain);
+        running = playAgain == 'y' ? true : false;
+    } while (running);
+    return 0;
+}
